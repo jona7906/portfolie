b613a7b36ae8691543fa4ec3c5e4ac7ffa44b109
jona7906 <70849321+jona7906@users.noreply.github.com>
2021-01-11 02:37:45 +0100

    web sektionen

--- javascript.js
+++ javascript.js
@@ -1,6 +1,115 @@
-document.querySelector("#nav_indledning").addEventListener("mousedown", visSection);
+document.querySelector("#nav_indledning").addEventListener("mousedown", visIndledning);
 
-function visSection() {
-    console.log("vis section");
+
+function visIndledning() {
+    console.log("vis indledning");
+
+    //skjul alle andre sektioner
+    document.querySelector("#portfolie_web").classList.remove("vis_section");
+    document.querySelector("#portfolie_ux").classList.remove("vis_section");
+    document.querySelector("#portfolie_indhold").classList.remove("vis_section");
+    document.querySelector("#portfolie_animation").classList.remove("vis_section");
+
+    document.querySelector("#portfolie_web").classList.add("hide_section");
+    document.querySelector("#portfolie_ux").classList.add("hide_section");
+    document.querySelector("#portfolie_indhold").classList.add("hide_section");
+    document.querySelector("#portfolie_animation").classList.add("hide_section");
+
+
+    //Vis indledning
+    //Fjern hide
     document.querySelector("#portfolie_indledning").classList.add("vis_section");
+    document.querySelector("#portfolie_indledning").classList.remove("hide_section");
+}
+
+
+
+document.querySelector("#nav_web").addEventListener("mousedown", visWeb);
+
+
+function visWeb() {
+    console.log("vis web");
+
+    //skjul alle andre sektioner
+    document.querySelector("#portfolie_indledning").classList.remove("vis_section");
+    document.querySelector("#portfolie_ux").classList.remove("vis_section");
+    document.querySelector("#portfolie_indhold").classList.remove("vis_section");
+    document.querySelector("#portfolie_animation").classList.remove("vis_section");
+
+    document.querySelector("#portfolie_indledning").classList.add("hide_section");
+    document.querySelector("#portfolie_ux").classList.add("hide_section");
+    document.querySelector("#portfolie_indhold").classList.add("hide_section");
+    document.querySelector("#portfolie_animation").classList.add("hide_section");
+    //Vis indledning
+    //Fjern hide
+    document.querySelector("#portfolie_web").classList.add("vis_section");
+    document.querySelector("#portfolie_web").classList.remove("hide_section");
+}
+
+
+document.querySelector("#nav_ux").addEventListener("mousedown", visUx);
+
+
+function visUx() {
+    console.log("vis ux");
+
+    //skjul alle andre sektioner
+    document.querySelector("#portfolie_indledning").classList.remove("vis_section");
+    document.querySelector("#portfolie_web").classList.remove("vis_section");
+    document.querySelector("#portfolie_indhold").classList.remove("vis_section");
+    document.querySelector("#portfolie_animation").classList.remove("vis_section");
+
+    document.querySelector("#portfolie_indledning").classList.add("hide_section");
+    document.querySelector("#portfolie_web").classList.add("hide_section");
+    document.querySelector("#portfolie_indhold").classList.add("hide_section");
+    document.querySelector("#portfolie_animation").classList.add("hide_section");
+    //Vis indledning
+    //Fjern hide
+    document.querySelector("#portfolie_ux").classList.add("vis_section");
+    document.querySelector("#portfolie_ux").classList.remove("hide_section");
+}
+
+document.querySelector("#nav_animation").addEventListener("mousedown", visAnimation);
+
+
+function visAnimation() {
+    console.log("vis animation");
+
+    //skjul alle andre sektioner
+    document.querySelector("#portfolie_indledning").classList.remove("vis_section");
+    document.querySelector("#portfolie_web").classList.remove("vis_section");
+    document.querySelector("#portfolie_indhold").classList.remove("vis_section");
+    document.querySelector("#portfolie_ux").classList.remove("vis_section");
+
+    document.querySelector("#portfolie_indledning").classList.add("hide_section");
+    document.querySelector("#portfolie_web").classList.add("hide_section");
+    document.querySelector("#portfolie_indhold").classList.add("hide_section");
+    document.querySelector("#portfolie_ux").classList.add("hide_section");
+    //Vis indledning
+    //Fjern hide
+    document.querySelector("#portfolie_animation").classList.add("vis_section");
+    document.querySelector("#portfolie_animation").classList.remove("hide_section");
+}
+
+
+document.querySelector("#nav_indhold").addEventListener("mousedown", visIndhold);
+
+
+function visIndhold() {
+    console.log("vis indhold");
+
+    //skjul alle andre sektioner
+    document.querySelector("#portfolie_indledning").classList.remove("vis_section");
+    document.querySelector("#portfolie_web").classList.remove("vis_section");
+    document.querySelector("#portfolie_animation").classList.remove("vis_section");
+    document.querySelector("#portfolie_ux").classList.remove("vis_section");
+
+    document.querySelector("#portfolie_indledning").classList.add("hide_section");
+    document.querySelector("#portfolie_web").classList.add("hide_section");
+    document.querySelector("#portfolie_animation").classList.add("hide_section");
+    document.querySelector("#portfolie_ux").classList.add("hide_section");
+    //Vis indledning
+    //Fjern hide
+    document.querySelector("#portfolie_indhold").classList.add("vis_section");
+    document.querySelector("#portfolie_indhold").classList.remove("hide_section");
 }
