2e44f200bf6ea1925bb00622d26a1bb01cb2e384
jona7906 <70849321+jona7906@users.noreply.github.com>
2021-01-12 19:23:27 +0100

    alt inhold i portfolie

--- javascript.js
+++ javascript.js
@@ -5,6 +5,117 @@ function startSide() {
     window.
 }*/
 
+
+let currentView;
+let nextView;
+let previousView;
+
+currentView = 1; //nuværrende billede
+newView = 0; //næste billede
+
+
+
+//skjuler alle billeder
+
+document.querySelector("#billede2").classList.add("skjul_billede");
+document.querySelector("#billede3").classList.add("skjul_billede");
+document.querySelector("#billede4").classList.add("skjul_billede");
+document.querySelector("#billede5").classList.add("skjul_billede");
+
+
+//tilføj eventlistener til knapper: næste/tidligere
+document.querySelector("#next_button").addEventListener("click", nextPicture);
+
+document.querySelector("#previous_button").addEventListener("click", previousPicture);
+
+//næste billede
+function nextPicture() {
+
+    //fjern eventlistener fra næste knap
+    document.querySelector("#next_button").removeEventListener("click", nextPicture);
+
+    if (currentView < 5) {
+        console.log("hvis nuværrende er under 8: + 1");
+        newView = currentView + 1;
+    }
+
+    if (currentView == 5) {
+        console.log("hvis 8, næste billed er 1");
+        newView = 1;
+    }
+    if (currentView > 5) {
+        console.log("hvis over 8, billed er 1");
+        currentView = 1;
+    }
+    if (currentView == 0) {
+        console.log("hvis 0, er nuværende 8");
+        currentView = 5;
+    }
+    //consol log
+    console.log("næste billede" + " billedet før: " + currentView + " nye billede: " + newView);
+
+    document.querySelector("#billede" + newView).classList.remove("skjul_billede");
+
+    document.querySelector("#billede" + newView).classList.add("vis_billede");
+
+    document.querySelector("#billede" + currentView).classList.add("skjul_billede");
+
+    document.querySelector("#billede" + currentView).classList.remove("vis_billede");
+
+    currentView++;
+    newView++;
+
+    document.querySelector("#next_button").addEventListener("click", nextPicture);
+}
+
+
+function previousPicture() {
+    //fjern eventlistener
+    document.querySelector("#previous_button").removeEventListener("click", previousPicture);
+
+    if (currentView > 1) {
+        console.log(" ");
+        newView = currentView - 1;
+
+    }
+
+    if (currentView == 1) {
+        console.log("if current is 1, new is 8");
+        newView = 5;
+    }
+
+    if (currentView == 0) {
+        console.log("if current is 0, current is 8");
+        currentView = 5;
+    }
+    if (currentView > 5) {
+        console.log("if curent is more than 8, current is 1");
+        currentView = 1;
+    }
+
+
+    console.log("prev image function" + " image before was: " + currentView + " new image is: " + newView);
+
+    document.querySelector("#billede" + newView).classList.remove("skjul_billede");
+
+    document.querySelector("#billede" + newView).classList.add("vis_billede");
+
+    document.querySelector("#billede" + currentView).classList.add("skjul_billede");
+
+    document.querySelector("#billede" + currentView).classList.remove("vis_billede");
+
+    newView--;
+
+    currentView--;
+
+    //tilføj  eventlistener igen
+    document.querySelector("#previous_button").addEventListener("click", previousPicture);
+
+}
+
+
+
+
 document.querySelector("#nav_indledning").addEventListener("mousedown", visIndledning);
 
 
@@ -126,4 +237,38 @@ function visIndhold() {
     //Fjern hide
     document.querySelector("#portfolie_indhold").classList.add("vis_section");
     document.querySelector("#portfolie_indhold").classList.remove("hide_section");
+}
+
+document.querySelector("#nav_om_indledning").addEventListener("mousedown", visOmmig);
+
+
+function visOmmig() {
+    console.log("vis om mig");
+
+    //skjul alle andre sektioner
+    document.querySelector("#mig_cv").classList.remove("vis_section");
+
+    document.querySelector("#mig_cv").classList.add("hide_section");
+
+    //Vis indledning
+    //Fjern hide
+    document.querySelector("#mig_om").classList.add("vis_section");
+    document.querySelector("#mig_om").classList.remove("hide_section");
+}
+
+document.querySelector("#nav_cv").addEventListener("mousedown", visOmcv);
+
+
+function visOmcv() {
+    console.log("vis om cv");
+
+    //skjul alle andre sektioner
+    document.querySelector("#mig_om").classList.remove("vis_section");
+
+    document.querySelector("#mig_om").classList.add("hide_section");
+
+    //Vis indledning
+    //Fjern hide
+    document.querySelector("#mig_cv").classList.add("vis_section");
+    document.querySelector("#mig_cv").classList.remove("hide_section");
 }
